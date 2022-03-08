c64c8669f0dd10067b67d43cc9ac99ad9ca237a9
Gabriel Queiroz <gabriel.queiroz@softecnologia.com.br>
2022-03-08 14:52:18 -0300

    73. Tornando o header dinâmico

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,41 @@
 const GET_LANDING_PAGE = /* GraphQL */ `
+  fragment imageProps on UploadFile {
+    alternativeText
+    url
+  }
+
+  fragment logo on LandingPage {
+    logo {
+      ...imageProps
+    }
+  }
+
+  fragment buttonProps on ComponentPageButton {
+    label
+    url
+  }
+
+  fragment button on ComponentPageHeader {
+    button {
+      ...buttonProps
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      ...button
+      image {
+        ...imageProps
+      }
+    }
+  }
+
   query GET_LANDING_PAGE {
     landingPage {
-      logo {
-        alternativeText
-        url
-      }
+      ...logo
+      ...header
     }
   }
 `
